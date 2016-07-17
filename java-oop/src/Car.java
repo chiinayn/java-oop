8703ea713e4228d65e66f331e3a39b78bbba7cdc
min_pc <min_pc@192.168.100.106>
2016-07-17 12:55:32 +0900

    car test

---
+++ java-oop/src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+	
+	int doors = 4;
+	int tires = 4;
+	boolean auto = true;
+	
+	public void drive() {
+		System.out.println("Drive중");
+	}
+	
+	public void back() {
+		System.out.println("빠꾸중");
+	}
+	
+	
+}
